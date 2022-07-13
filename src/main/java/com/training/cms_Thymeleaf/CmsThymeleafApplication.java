6c6e682beccbdde951d6caf9b8a510f2db2945c0
thespeace <thespeace1@gmail.com>
2022-07-13 18:26:54 +0900

    start

---
+++ src/main/java/com/training/cms_Thymeleaf/CmsThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.training.cms_Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/cms_Thymeleaf/CmsThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.cms_Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
